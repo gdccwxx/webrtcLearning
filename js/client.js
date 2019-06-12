c47dec7ab5b4e5f9757939e0c7a04d2b1a895466
gdccwxx <765553928@qq.com>
2019-06-12 18:10:04 +0800

    autocommit

--- js/client.js
+++ js/client.js
@@ -1,7 +1,27 @@
 'use strict'
+var audioSource = document.querySelector('select#audioSource')
+var audioOutput = document.querySelector('select#audioOutput')
+var vidioSource = document.querySelector('select#vidioSource')
+
 var videoPlayer = document.querySelector('video#player')
 function gotMediaStream(stream) {
     videoPlayer.srcObject = stream
+    return navigator.mediaDevices.enumerateDevices();
+}
+function gotDevices(deviceInfos) {
+    deviceInfos.forEach(deviceInfo => {
+        var option = document.createElement('option')
+        option.text = deviceInfo.label
+        option.value = deviceInfo.deviceId
+
+        if (deviceInfo.kind === 'audioinput') {
+            audioSource.appendChild(option)
+        } else if (deviceInfo.kind === 'audiooutput') {
+            audioOutput.appendChild(option)
+        } else if (deviceInfo.kind === 'videoinput') {
+            vidioSource.appendChild(option)
+        }
+    });
 }
 function handleError(err) {
     console.log('getUserMedia err: '+ err)
@@ -15,5 +35,6 @@ if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
     }
     navigator.mediaDevices.getUserMedia(contrants)
     .then(gotMediaStream)
+    .then(gotDevices)
     .catch(handleError)
 }
